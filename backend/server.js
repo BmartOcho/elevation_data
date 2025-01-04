e7b613f767e9563879a6dff8df361058d729027c
BmartOcho <ben.martinec@gmail.com>
2025-01-04 15:55:08 -0600

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+// Test route
+app.get("/", (req, res) => {
+    res.send("Backend is running!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from "react";
+
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000")
+    .then((response) => response.text())
+    .then((data) => setMessage(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Equipment Rental App</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+export default App;
