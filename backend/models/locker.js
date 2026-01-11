5ff0d7a12eaa992d2f32331bb797079e947d8708
Benjamin Martinec <109639555+BmartOcho@users.noreply.github.com>
2026-01-11 09:40:20 -0600

    Delete backend directory

--- backend/models/locker.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const LockerSchema = new mongoose.Schema({
-  location: { type: String, required: true },
-  equipment: { type: [String], required: true }, // Array of equipment names
-  status: { type: String, default: "available" }, // "available" or "rented"
-});
-
-module.exports = mongoose.model("Locker", LockerSchema);
